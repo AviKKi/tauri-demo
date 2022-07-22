af1dde85b3d2bbc9c04ce7d1e8f552598a195ad6
Atul Yadav <atul7555@gmail.com>
2022-07-23 04:25:33 +0530

    Counter Example Done

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,10 +1,32 @@
 #![cfg_attr(
-  all(not(debug_assertions), target_os = "windows"),
-  windows_subsystem = "windows"
+    all(not(debug_assertions), target_os = "windows"),
+    windows_subsystem = "windows"
 )]
 
+use std::sync::Mutex;
+
+#[tauri::command]
+fn get_count(state: tauri::State<AppState>) -> i64 {
+    state.count.lock().unwrap().clone()
+}
+
+
+#[tauri::command]
+fn update_count(update: i64, state: tauri::State<AppState>) -> i64 {
+    let mut cnt = state.count.lock().unwrap();
+    *cnt += update;
+    cnt.clone()
+}
+
+struct AppState {
+    count: Mutex<i64>,
+}
+
 fn main() {
-  tauri::Builder::default()
-    .run(tauri::generate_context!())
-    .expect("error while running tauri application");
+    let mut state = AppState { count: Default::default() };
+    tauri::Builder::default()
+        .manage(state)
+        .invoke_handler(tauri::generate_handler![get_count, update_count])
+        .run(tauri::generate_context!())
+        .expect("error while running tauri application");
 }
--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,53 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { invoke } from '@tauri-apps/api';
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Link,
+} from "react-router-dom";
 
-function App() {
+
+const Counter = () => {
+  const [count, setCount] = useState(0)
+  useEffect(() => {
+    invoke('get_count').then((c: any) => setCount(c))
+  }, [])
+  const inc = () => {
+    invoke('update_count', { update: 1 }).then((c: any) => setCount(c))
+  }
+  const dec = () => {
+    invoke('update_count', { update: -1 }).then((c: any) => setCount(c))
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Edit Test
-        </a>
-      </header>
+      <button onClick={dec}>-</button>
+      <input value={count} />
+      <button onClick={inc}>+</button>
     </div>
   );
+}
+
+const Index = () => {
+  return <ul>
+    <Link to="/counter">Counter</Link>
+  </ul>
+}
+
+function App() {
+  return <div>
+    <BrowserRouter>
+      <Link to="/">All Apps</Link>
+
+      <Routes>
+        <Route path="/" element={<Index />} />
+        <Route path="/counter" element={<Counter />} />
+      </Routes>
+    </BrowserRouter>
+  </div>
+  return <Counter />
 }
 
 export default App;
